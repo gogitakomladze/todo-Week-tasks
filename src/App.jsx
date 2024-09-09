db3aedcb10a06f25a4dba900640e16ac70e73713
gogitakomladze <komladzemixeili@gmail.com>
2024-09-09 23:50:04 +0400

    Body

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { Header } from "./header"
+import { Body } from "./body"
+
 function App() {
 
   return (
     <>
   <Header/>
+  <Body/>
+  
     </>
   )
 }
---
+++ src/body/Body.jsx
@@ -0,0 +1,7 @@
+export function Body() {
+    return (
+        <div>
+            <h1>Body</h1>
+        </div>
+    )
+}
